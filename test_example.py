243bb6edd2f8858cb3a6d08f2325c6673a8e681e
chaocw <chaoc.w@outlook.com>
2024-12-14 01:50:25 +0800

    init

---
+++ test_example.py
@@ -0,0 +1,15 @@
+import allure
+
+@allure.title("Test Authentication")
+@allure.description("This test attempts to log into the website using a login and a password. Fails if any error happens.\n\nNote that this test does not test 2-Factor Authentication.")
+@allure.tag("NewUI", "Essentials", "Authentication")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.label("owner", "John Doe")
+@allure.link("https://dev.example.com/", name="Website")
+@allure.issue("AUTH-123")
+@allure.testcase("TMS-456")
+@allure.parent_suite("Tests for web interface")
+@allure.suite("Tests for essential features")
+@allure.sub_suite("Tests for authentication")
+def test_authentication():
+    ...
